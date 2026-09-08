954e5bc81ac57d25e483811689d04711c90ba482
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2026-09-08 12:08:31 -0500

    estados-scalamin

--- src/app/Components/Estado/seleccion-proceso-estatos-dialog/seleccion-proceso-estatos-dialog.component.ts
+++ src/app/Components/Estado/seleccion-proceso-estatos-dialog/seleccion-proceso-estatos-dialog.component.ts
@@ -12,7 +12,7 @@ import { OpcionesDialogComponent } from '../opciones-dialog/opciones-dialog.comp
   styleUrl: './seleccion-proceso-estatos-dialog.component.css'
 })
 export class SeleccionProcesoEstatosDialogComponent {
-  procesos = [ 'PERFORACIÓN TALADROS LARGOS', 'PERFORACIÓN HORIZONTAL', 'EMPERNADOR', 'ACARREO',  'SCOOPTRAM', 'DUMPER'];
+  procesos = [ 'PERFORACIÓN TALADROS LARGOS', 'PERFORACIÓN HORIZONTAL', 'EMPERNADOR',  'SCOOPTRAM', 'SCALAMIN', 'DUMPER', 'ACARREO'];
 
   constructor(
     public dialogRef: MatDialogRef<SeleccionProcesoEstatosDialogComponent>,
